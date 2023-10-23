d5c5dfcbd216a98921a167f0dd3ca5e53d95a172
Cthupby <Cthupby@gmail.com>
2023-10-23 12:19:32 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,16 +6,17 @@ from db import Base
 class Question(Base):
     """
     Модель Вопроса.
-    Колонки соответсвуют основным полям вопроса из API.
+    Колонки соответствуют основным полям вопроса из API.
     """
     __tablename__ = "questions"
 
-    id = Column(
+    order_id = Column(
         Integer,
         autoincrement=True,
         primary_key=True,
         index=True,
     )
+    id = Column(Integer)
     answer = Column(Text)
     question = Column(Text)
     value = Column(Integer)
