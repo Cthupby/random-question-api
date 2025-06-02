903c9b56554cdc36df2f078cf9bd2ef61f28a6f5
Sergey <Cthupby@gmail.com>
2025-06-03 00:00:39 +0300

    Update tests for services

---
+++ tests/test_services.py
@@ -0,0 +1,12 @@
+"""
+TODO:
+
+Необходимо добавить тесты для:
+- create_question,
+- get_questions,
+- get_question_by_id,
+- get_last_question,
+- request_new_questions,
+- create_new_questions,
+- get_new_questions(
+"""
